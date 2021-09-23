ba8055ed1171a10a72fc08f8867ad0485a535d24
Mike <nnaattvvaall1@gmail.com>
2021-09-23 11:32:37 +0300

    added word counter

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+import re
 
 def home(request):
     return render(request, 'home.html')
 
 def reverse(request):
     user_text= request.GET["usertext"]
+    le = user_text.split()
+    counter = len(le)
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html',{'usertext':user_text,'reversedtext':reversed_text})
+
+    return render(request, 'reverse.html',{'usertext':user_text,'reversedtext':reversed_text,'counter':counter})
